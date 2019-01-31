73801d429b59c739726bd69bd253cef411dc9846
lindsayfayeh <47042292+lindsayfayeh@users.noreply.github.com>
2019-01-31 16:09:28 -0500

    Complete Homework 2

---
+++ Homework2.R
@@ -0,0 +1,64 @@
+#clear workspace
+rm(list=ls())
+#read csv file in
+RedKnot<-read.csv(file="RK_ShorebirdSurveys.csv",head=TRUE,sep=",")
+
+names(RedKnot)
+#reformatting date as YEAR MONTH DAY.
+RedKnot$Date <- as.POSIXct(strptime(RedKnot$Date, '%Y-%m-%d', tz = 'EST'))
+
+#install packages 
+#install.packages("ggplot2")
+#install.packages("tidyverse")
+#install.packages("dplyr")
+#install.packages("magrittr")
+#install.packages("reshape2")
+#install.packages("reshape")
+#install.packages("gridExtra")
+#install.packages("knitr")
+#install.packages("officer")
+
+#library call
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(magrittr)
+library(reshape)
+library(reshape2)
+library(gridExtra)
+library(knitr)
+library(officer)
+
+#basic calls and calcs for data
+unique(RedKnot$Subsite)
+str(RedKnot)
+head(RedKnot)
+tail(RedKnot)
+log10(RedKnot$Unbanded)
+dim(RedKnot)
+
+#tallying number of REKN seen each date surveyed, row sum script example
+#rowSums(dat[,c("b", "c")], na.rm=TRUE)
+RedKnot$total = rowSums(RedKnot[,c("Ocean","BayBackshore")],na.rm=T)
+
+##want to know how many redknots for each date per subsite
+sum.red.knots = aggregate(total~Subsite+Date, FUN=sum, data=RedKnot)%>%
+  arrange(Date)
+
+View(sum.red.knots)
+
+#spreading data by subsite
+REKN.wide<-spread(sum.red.knots, Subsite, total, convert = T)
+View(REKN.wide)
+
+#Mean of REKN at each subsite throughout the study period, summarize creates new dataframe
+  mean.by.subsite = sum.red.knots %>%
+  group_by(Subsite) %>%
+    summarise(total=mean(total, na.rm = TRUE))
+  
+#Mutate to add column and create new dataframe
+  total.mean.bay = RedKnot %>%
+    group_by(Subsite, Date, BayBackshore) %>%
+    mutate(total.mean.bay=mean(BayBackshore, na.rm = TRUE))
+
+  
